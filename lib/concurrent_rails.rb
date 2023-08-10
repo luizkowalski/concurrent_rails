c36752978e7b1622675e9cdcd0405479fcb7b6a0
Luiz Eduardo Kowalski <luizeduardokowalski@gmail.com>
2023-08-10 23:32:00 +0200

    Zeitwerk (#36)

--- lib/concurrent_rails.rb
+++ lib/concurrent_rails.rb
@@ -1,8 +1,5 @@
 # frozen_string_literal: true
 
-require "concurrent_rails/future"
-require "concurrent_rails/multi"
-require "concurrent_rails/promises"
-require "concurrent_rails/railtie"
-require "concurrent_rails/testing"
-require "concurrent_rails/version"
+require "zeitwerk"
+loader = Zeitwerk::Loader.for_gem
+loader.setup
