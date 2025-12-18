5e73139defdeff0f7a6127cf1e2845db18df2298
Luiz Eduardo Kowalski <luizeduardokowalski@gmail.com>
2025-12-18 16:12:34 +0100

    put appraisal file back

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,9 +3,20 @@
 # Configure Rails Environment
 ENV["RAILS_ENV"] = "test"
 
+puts "here"
 require_relative "../test/dummy/config/environment"
-require "minitest/reporters"
+ActiveRecord::Migrator.migrations_paths = [File.expand_path("../test/dummy/db/migrate", __dir__)]
 require "rails/test_help"
+
+# Load fixtures from the engine
+if ActiveSupport::TestCase.respond_to?(:fixture_paths=)
+  ActiveSupport::TestCase.fixture_paths = [File.expand_path("fixtures", __dir__)]
+  ActionDispatch::IntegrationTest.fixture_paths = ActiveSupport::TestCase.fixture_paths
+  ActiveSupport::TestCase.file_fixture_path = "#{File.expand_path('fixtures', __dir__)}/files"
+  ActiveSupport::TestCase.fixtures :all
+end
+
+require "minitest/reporters"
 require "rails/test_unit/reporter"
 
 Minitest::Reporters.use!
