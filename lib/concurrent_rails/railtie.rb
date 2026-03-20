a7b2decf01f8007cc09e472eb8e1556beef507b4
Luiz Eduardo Kowalski <luizeduardokowalski@gmail.com>
2026-03-21 00:39:44 +0100

    remove useless code

--- lib/concurrent_rails/railtie.rb
+++ lib/concurrent_rails/railtie.rb
@@ -3,8 +3,7 @@
 module ConcurrentRails
   class Railtie < ::Rails::Railtie
     initializer "concurrent_rails.executor_hooks" do |app|
-      app.executor.to_run { nil }
-      app.executor.to_complete { nil }
+      # TODO: Add something here at some point
     end
   end
 end
