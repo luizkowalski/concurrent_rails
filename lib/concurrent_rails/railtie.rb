a8e209a89948ebce5e9e9ac67a03da1e6091816f
Luiz Eduardo Kowalski <luizeduardokowalski@gmail.com>
2026-03-21 00:37:49 +0100

    Fix empty block rubocop offenses in Railtie

--- lib/concurrent_rails/railtie.rb
+++ lib/concurrent_rails/railtie.rb
@@ -3,8 +3,8 @@
 module ConcurrentRails
   class Railtie < ::Rails::Railtie
     initializer "concurrent_rails.executor_hooks" do |app|
-      app.executor.to_run {}
-      app.executor.to_complete {}
+      app.executor.to_run { nil }
+      app.executor.to_complete { nil }
     end
   end
 end
